64b32deedd47767cab4aff34cd5f245311a60afd
Lucas Shaw <lgs84@cornell.edu>
2020-06-16 13:59:21 -0700

    sins.c ok

--- src/libultra_code/sins.c
+++ src/libultra_code/sins.c
@@ -1,3 +1,13 @@
-#include "libultra_internal.h"
+#include "sintable.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/sins/sins.s")
+s16 sins(u16 angle) {
+    s16 value;
+    angle >>= 4;
+    
+    value = (angle & 0x400) ? sintable[0x3FF - (angle & 0x3FF)] : sintable[angle & 0x3FF];
+    
+    if (angle & 0x800) {
+        return -value;
+    }
+    return value;
+}
