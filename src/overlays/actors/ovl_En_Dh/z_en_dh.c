4f9c1d5ff218509e0e5e0c3ff4b046b557b8b1a0
Lucas Shaw <lgs84@cornell.edu>
2020-07-15 01:55:08 -0700

    decompiled data in z_en_dh

--- src/overlays/actors/ovl_En_Dh/z_en_dh.c
+++ src/overlays/actors/ovl_En_Dh/z_en_dh.c
@@ -9,7 +9,16 @@ void EnDh_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnDh_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnDh_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void func_809EB110(EnDh* this, GlobalContext* globalCtx);
+void func_809EB3D0(EnDh* this, GlobalContext* globalCtx);
+void func_809EB53C(EnDh* this, GlobalContext* globalCtx);
+void func_809EB624(EnDh* this, GlobalContext* globalCtx);
+void func_809EB9B8(EnDh* this, GlobalContext* globalCtx);
+void func_809EBBC8(EnDh* this, GlobalContext* globalCtx);
+void func_809EBD78(EnDh* this, GlobalContext* globalCtx);
+void func_809EBEA8(EnDh* this, GlobalContext* globalCtx);
+void func_809EC1A0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, EnDh* this, Gfx** gfx);
+
 const ActorInit En_Dh_InitVars = {
     ACTOR_EN_DH,
     ACTORTYPE_ENEMY,
@@ -21,7 +30,47 @@ const ActorInit En_Dh_InitVars = {
     (ActorFunc)EnDh_Update,
     (ActorFunc)EnDh_Draw,
 };
-*/
+
+ColliderCylinderInit D_809EC5C0 = 
+{
+    { COLTYPE_UNK0, 0x00, 0x0D, 0x09, 0x10, COLSHAPE_CYLINDER },
+    { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000008, 0x00, 0x00 }, 0x00, 0x01, 0x01 },
+    { 35, 70, 0, { 0, 0, 0 } },
+};
+
+ColliderJntSphItemInit D_809EC5EC[1] = {
+    {
+        { 0x00, { 0x00000000, 0x00, 0x00 }, { 0xFFCFFFFF, 0x00, 0x00 }, 0x00, 0x01, 0x09 },
+        { 1, { { 0, 0, 0 }, 20 }, 100 },
+    },
+};
+
+ColliderJntSphInit D_809EC610 = 
+{
+    { COLTYPE_UNK6, 0x00, 0x09, 0x09, 0x10, COLSHAPE_JNTSPH },
+    1, D_809EC5EC,
+};
+
+DamageTable D_809EC620 = {
+	0x00, 0xF2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0xF2, 0xF2, 0xF4, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF2, 0xF4,
+	0xF2, 0xF4, 0xF8, 0xF4, 0x00, 0x00, 0xF4, 0x00,
+};
+
+InitChainEntry D_809EC640[] = {
+    ICHAIN_S8(naviEnemyId, 47, ICHAIN_CONTINUE),
+    ICHAIN_F32(unk_4C, 2000, ICHAIN_CONTINUE),
+    ICHAIN_VEC3F_DIV1000(scale, 10, ICHAIN_CONTINUE),
+    ICHAIN_F32_DIV1000(gravity, 62036, ICHAIN_STOP),
+};
+
+Vec3f D_809EC650 = { 0.0f, 8.0f, 0.0f };
+
+Vec3f D_809EC65C = { 0.0f, -1.5, 0.0f };
+
+Vec3f D_809EC668 = { 2000.0f, 1000.0f, 0.0f };
+
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Dh/func_809EAD40.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Dh/EnDh_Init.s")
--- src/overlays/actors/ovl_En_Dh/z_en_dh.h
+++ src/overlays/actors/ovl_En_Dh/z_en_dh.h
@@ -6,9 +6,29 @@
 
 struct EnDh;
 
+typedef void (*EnDhActionFunc)(struct EnDh*, GlobalContext*);
+
 typedef struct EnDh {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x1D8];
+    /* 0x014C */ SkelAnime skelAnime;
+    /* 0x0190 */ Vec3s limbDrawTable[16];
+    /* 0x01F0 */ Vec3s transitionDrawTable[16];
+    /* 0x0250 */ u8 unk_250;
+    /* 0x0251 */ char unk_251;
+    /* 0x0252 */ u8 unk_252;
+    /* 0x0253 */ char unk_253;
+    /* 0x0254 */ EnDhActionFunc actionFunc;
+    /* 0x0258 */ u8 unk_258;
+    /* 0x0259 */ char unk_259;
+    /* 0x025A */ u8 unk_25A;
+    /* 0x025B */ char unk_25B;
+    /* 0x025C */ s16 unk_25C;
+    /* 0x025E */ s16 unk_25E;
+    /* 0x0260 */ ColliderCylinder collider;
+    /* 0x02AC */ ColliderJntSph colliderJntSph;
+    /* 0x02CC */ ColliderJntSphItem colliderJntSphItem;
+    /* 0x030C */ Vec3f vec_30C;
+    /* 0x0318 */ Vec3f vec_318;
 } EnDh; // size = 0x0324
 
 extern const ActorInit En_Dh_InitVars;
